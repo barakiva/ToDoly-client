19def699fd0dda2750a21bbd15bcbfa5545a7a7a
Bar Akiva <barakiva1@gmail.com>
2020-01-05 14:37:47 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ScheduleListComponent } from './schedule-list/schedule-list.component';
+import { FabComponent } from './fab/fab.component';
+import { MainWorkspaceComponent } from './main-workspace/main-workspace.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ScheduleListComponent,
+    FabComponent,
+    MainWorkspaceComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-workspace/main-workspace.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-workspace',
+  templateUrl: './main-workspace.component.html',
+  styleUrls: ['./main-workspace.component.css']
+})
+export class MainWorkspaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
